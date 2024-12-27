8f6469fff1c68766a52ad7916f6f45f0b4a57c97
mittag.tech <mittag.tech@gmail.com>
2024-12-27 19:42:51 +0900

    feature: run cybergear.

--- src/m5ros.ino
+++ src/m5ros.ino
@@ -1,6 +1,5 @@
 //
-// micro-ROS subscriber sample program
-// for M5Stack
+// micro-ROS subscriber program for CyberGear Controller by M5Stack
 //
 #include <M5Stack.h>
 #include <micro_ros_arduino.h>
@@ -13,6 +12,12 @@
 
 #include <std_msgs/msg/int32.h>
 
+#include <mcp_can.h>
+#include "cybergear_controller.hh"
+#include "cybergear_can_interface_esp32.hh"
+#include "cybergear_can_interface_mcp.hh"
+
+// micro-ROS settings
 rcl_subscription_t subscriber;
 std_msgs__msg__Int32 msg;
 rclc_executor_t executor;
@@ -21,44 +26,80 @@ rcl_allocator_t allocator;
 rcl_node_t node;
 rcl_timer_t timer;
 
-//#define LED_PIN 13
+// Cybergear settings
+// setup master can id and motor can id (default cybergear can id is 0x7F)
+uint8_t MASTER_CAN_ID = 0x00;
+std::vector<uint8_t> motor_ids = {0x7F};
+std::vector<float> speeds = {0.0f};
+#ifdef USE_ESP32_CAN
+CybergearCanInterfaceEsp32 interface;
+#else
+CybergearCanInterfaceMcp CAN0;
+#endif
+CybergearController controller = CybergearController(MASTER_CAN_ID);
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){check_connect();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
-
-void error_loop(){
-  M5.Lcd.print("Error!!\n");  
-  //while(1){
-  //  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-  //  delay(1000);
-  //}
+void check_connect(){
+  // update m5 status
+  M5.Lcd.print("connecting...");
 }
 
 void subscription_callback(const void * msgin)
 {  
   const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
-  //digitalWrite(LED_PIN, (msg->data == 0) ? LOW : HIGH);
-  M5.Lcd.printf("Data: %d\n", msg->data);  
+  M5.Lcd.printf("Data: %d\n", msg->data);
+    switch(msg->data) {
+    case 1:  // 上矢印キー
+      controller.send_position_command(0x7F, 10.0f);  // 正回転（速度は10 rad/s）
+      M5.Lcd.setCursor(0, 100);
+      M5.Lcd.println("Forward rotation   ");
+      break;
+      
+    case 2:  // 下矢印キー
+      controller.send_position_command(0x7F, -10.0f);  // 逆回転（速度は-10 rad/s）
+      M5.Lcd.setCursor(0, 100);
+      M5.Lcd.println("Reverse rotation   ");
+      break;
+      
+    default:  // その他のキー
+      controller.send_position_command(0x7F, 0.0f);  // 停止
+      M5.Lcd.setCursor(0, 100);
+      M5.Lcd.println("Stop              ");
+      break;
+  }
 }
 
 void setup() {
+  // initialize M5
   M5.begin();
   M5.Power.begin();
   M5.Lcd.setTextSize(2); 
-  M5.Lcd.print("Hello micro-ROS\n");
-  M5.Lcd.print("/micro_ros_arduino_subscriber\n");
+  M5.Lcd.clear(BLACK);
+  M5.Lcd.setCursor(0,0,2);
+  M5.Lcd.print("initialize start\n");
   
-  // for WiFi
-//   set_microros_wifi_transports("SSID", "PASS", "192.168.xxx.xxx", 8888);
-  // for USB
+  // initialize micro-ros
   set_microros_transports();
-  
-  //pinMode(LED_PIN, OUTPUT);
-  //digitalWrite(LED_PIN, HIGH);  
-  
-  delay(2000);
+  M5.Lcd.print("initialize micro-ros\n");
 
+  //initialize cybergear
+  Serial.begin(115200);
+  CAN0.init();
+  M5.Lcd.printf("initialize can\n");
+  controller.init(motor_ids, MODE_POSITION, &CAN0);
+  M5.Lcd.printf("initialize controller\n");
+  controller.enable_motors();
+  controller.send_position_command(motor_ids, speeds);
+  M5.Lcd.printf("initialize motor\n");
+
+  M5.Lcd.print("all initialize");
+  delay(1000);
+  M5.Lcd.clear(TFT_BLACK);
+  M5.Lcd.setCursor(0,0,2);
+
+  delay(2000);
   allocator = rcl_get_default_allocator();
 
   //create init_options
@@ -80,6 +121,10 @@ void setup() {
 }
 
 void loop() {
-  delay(100);
+  //update m5 status
+  M5.Lcd.print("Cybergear Controller\n");
   RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  delay(1000);
+  M5.Lcd.clear(TFT_BLACK);
+  M5.Lcd.setCursor(0,0,2);
 }
